69cd5d2259274f350644734998b72e626ab679bc
Stephen Parker <stephenparkhum@outlook.com>
2020-12-05 09:39:11 -0800

    Initial react setup

---
+++ src/components/Header/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <div class="hero">
+            <h1>Here's a hero</h1>
+        </div>
+    )
+};
---
+++ src/components/Header/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <nav>
+            <h1>Here's a nav</h1>
+        </nav>
+    )
+};
