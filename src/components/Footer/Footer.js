b81d72d62725402389c9f9610a8690cb72848edb
Stephen <stephenparkhum@outlook.com>
2020-12-18 22:16:44 -0800

    Updating styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function Footer() {
     return (
+        <>
         <footer>
             <div className="footer--left">
                 <ul>
@@ -9,15 +10,22 @@ export default function Footer() {
                     <li><span className="accent">USA Locations | </span><span className="grey-accent">Hawaii, Ohio, Tennessee, New York, Oregon, Utah, Texas, California</span></li>
                     <li><span className="accent">International Locations | </span><span className="grey-accent">Germany, Egypt</span></li>
                 </ul>
-                <p className="grey-accent">Copyright 2020 The Urban Collaborative</p>
             </div>
             <div className="footer--right">
             <ul>
-                    <li><span className="grey-accent">info@urbancollaborative.com</span></li>
-                    <li><span className="grey-accent">+1.510.551.8065</span></li>
-                    </ul>
-                <p className="grey-accent">Full Contact List</p>
+              <li><span className="grey-accent">info@urbancollaborative.com</span></li>
+              <li><span className="grey-accent">+1.510.551.8065</span></li>
+            </ul>
             </div>
         </footer>
+        <div className="footer--bottom">
+            <div className="">
+                  <p className="grey-accent">Copyright 2020 The Urban Collaborative</p>
+            </div>
+        <div className="">
+                <p className="grey-accent">Full Contact List</p>
+            </div>
+        </div>
+        </>
     )
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import philosophyImage from '../../images/All-Hands-HI-Cropped-e1433814849283.jpg';
 
 export default function Main() {
     return (
@@ -8,7 +9,8 @@ export default function Main() {
               <p><span className="accent">THE URBAN COLLABORATIVE, LLC</span> is an interdisciplinary design firm that focuses on helping our clients achieve their sustainability goals through engaging, educational, and efficient participatory processes tailored to each project’s scale, context, and culture. Our award-winning work addresses environmental, fiscal, and social sustainability in ways that support and enhance our clients’ missions. 
               <br /><br />While we complete private sector work for clients committed to advancing sustainability, most of our work is for U.S. federal agencies including the National Aeronautics and Space Administration and the Department of Defense. The Urban Collaborative also works on U.S. federal civil works projects prioritizing resilient systems by integrating regional master planning with capacity building among stakeholders. This includes teaching master planning PROSPECT courses to federal planners and practitioners. We have helped craft national policies, we teach from those policies, and we prepare plans aligned with those policies. Additionally, we focus on municipal planning for cities and counties throughout the country.</p>
           </div>
-          <div className="main--text-model">
+          <div className="main--text-model philosophy">
+              <img src={philosophyImage} />
               <h1>Philosophy</h1>
               <p><span className="accent">Collaboration.</span> The Urban Collaborative prioritizes working with our clients and stakeholders in all aspects of design and planning. We have pioneered a process that integrates the goals and needs of all involved in an engaging, efficient, and effective manner.
               <br /><br /><span className="accent">Sustainability.</span> Our goal is to create places that support and sustain an area’s or an installation’s diverse missions and that are environmentally, economically, and socially sustainable.
